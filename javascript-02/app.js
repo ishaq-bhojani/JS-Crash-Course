0d41861b12ac28699afbe87fadc778b087a41fb3
Ishaq Bhojani <ibhojani@nisum.com>
2023-04-04 23:34:34 +0500

    class 02 code

---
+++ javascript-02/app.js
@@ -0,0 +1,43 @@
+// let data = "Hello ";
+// let data2 = "Students";
+
+// let data3 = data + data2; // Strings Concatenation
+// console.log(data3);
+
+// num + num = addition
+// num + string = Concatenation
+// string + num = Concatenation
+// string + string = Concatenation
+
+// let data = "5";
+// let data2 = 5;
+
+// let data3 = data + data2; // addition
+// console.log(data3);
+
+// let data = "Hello Students";
+// let data2 = data.includes("Students");
+// console.log(data2);
+
+// Math Operators = + - * / %
+
+// let data = 10;
+// let data2 = 3;
+// console.log(data % data2);
+
+// let userFavNum = prompt("What is your favourite number?");
+// console.log(userFavNum);
+
+// let userName = prompt("What is your Name?");
+// document.write("Welcome to my website " + userName);
+
+// let randomNumber = Math.random();
+// // randomNumber = randomNumber * 10; 
+// randomNumber *= 10; // Short Hand
+// let roundedNumber = Math.round(randomNumber);
+// console.log(roundedNumber);
+
+// BODMAS = Brackets Of, Divide, Multiply, Add, Subtract
+let num = 5 ** 3;
+
+console.log(num);
