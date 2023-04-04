f37ebefe0d8cb5d94c3d009bac6dde561b2c9515
Ishaq Bhojani <ibhojani@nisum.com>
2023-04-04 19:26:05 +0500

    class 01 code

---
+++ javascript-01/app.js
@@ -0,0 +1,35 @@
+// let firstName = "Ishaq"; // Variable // var // let
+
+// Variable Rules
+// 1. Legal or Illegal names
+// Legal
+// let lastName // CamelCase
+// lastname
+// Allowed Special Characters
+// _ $
+// let $nameWith$Dollar = "";
+// let _nameWith_Dollar = "";
+// Var cannot be start with a number
+// let myName
+
+// Illegal
+// let last name // spaces
+// let my#name
+// reserved words
+
+// alert(firstName);
+
+// Data Types
+
+// 1. String
+let firstName = 'Ishaq'; 
+// examples = "Farrukh", "An Apple", "#", "8" "email@test.com"
+// Any letter, word or sentence is String in JavaScript
+
+// 2. Number
+let myLuckyNumber = 8;
+// example = 4, 37, 0, -34, 4.765
+
+// 3. Boolean
+let isTodayMonday = true;
+// emaple = true & false
