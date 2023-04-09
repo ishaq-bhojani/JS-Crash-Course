32add0f2159941a87219e5dc9d3c9383c2886745
Ishaq Bhojani <ibhojani@nisum.com>
2023-04-09 23:33:27 +0500

    class 07 code

---
+++ javascript-07/app.js
@@ -0,0 +1,50 @@
+// let myPara = document.getElementById("my-para");
+
+// myPara.innerText = "Hello";
+
+// let myPara = document.querySelector("p");
+// myPara.innerHTML = "<b>Pakistan Zindabad</b>"
+// console.log(myPara);
+
+// let myPara = document.querySelector("#my-para");
+// myPara.innerHTML = "<b>Pakistan Zindabad</b>";
+// myPara.style.backgroundColor = "#000";
+// myPara.style.color = "#fff";
+
+// setInterval(function() {
+//   if(myPara.style.display === "none") {
+//     myPara.style.display = "block";
+//   } else {
+//     myPara.style.display = "none";
+//   }
+// }, 500);
+
+// let yourPara = document.querySelector("#your-para");
+// yourPara.innerHTML = "<b>Pakistan Zindabad</b>";
+// yourPara.style.backgroundColor = "#000";
+// yourPara.style.color = "#fff";
+// yourPara.style.width = "100%";
+
+// setInterval(function () {
+//   let width = yourPara.style.width;
+//   if (width === "100%") {
+//     yourPara.style.width = "10%";
+//   } else { // "30%"
+//     width = +width.replace("%", ""); // "30%" => "30" => 30
+//     yourPara.style.width = width + 10 + "%"; // 30 + 10 = 40 + "%" = "40%""
+//   }
+// }, 500);
+
+// const myPara = document.getElementById("my-para");
+// myPara.innerHTML = "<b>Pakistan Zindabad</b>";
+// setInterval(function () {
+//   let color1 = Math.floor(Math.random()*16777215).toString(16);
+//   let color2 = Math.floor(Math.random()*16777215).toString(16);
+//   myPara.style.backgroundColor = "#" + color1;
+//   myPara.style.color = "#" + color2;
+// }, 500);
+
+let dataThatCanChange = "asdf";
+dataThatCanChange = "ghjkl";
+const dataThatCanNotBeChanged = "asdfghjk";
+dataThatCanNotBeChanged = "sadasd";
