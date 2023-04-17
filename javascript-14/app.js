0dc9f8c9f9d4d4c2052f3c523bea53e688c487d0
Ishaq Bhojani <ibhojani@nisum.com>
2023-04-17 23:28:35 +0500

    class 14 code correction

--- javascript-14/app.js
+++ javascript-14/app.js
@@ -1,7 +1,7 @@
 const quizSelector = document.getElementById("quiz-selector");
 const quizContainer = document.getElementById("quiz-container");
 const resultsContainer = document.getElementById("results-container");
-const questionContainer = document.getElementById("question");
+const questionContainer = document.getElementById("question-container");
 const answerButtonsContainer = document.getElementById(
   "answer-buttons-container"
 );
